804b0698abcbe9090953d165a696b23c86fa114e
moacirmoda <moa.moda@gmail.com>
2013-04-23 18:50:40 -0300

    comenting signals

--- bireme/main/signals.py
+++ bireme/main/signals.py
@@ -2,8 +2,11 @@ from django.db.models import signals
 from main.models import Network, CooperativeCenter, NetworkMembership
 
 def append_memberships(sender, instance, created, **kwargs):
+    """
+        If the network type is national and was included your country,
+        automatically appends all centers that have that country too.
+    """
     if created:
-        
         if instance.type == 'national' and instance.country:
             for cc in CooperativeCenter.objects.filter(country=instance.country):
                 nm = NetworkMembership(network=instance, cooperative_center=cc)
