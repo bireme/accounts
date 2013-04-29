f3d5aafc7d4e6220bbe0ecfdc772f5dfe4f475fb
moacirmoda <moa.moda@gmail.com>
2013-04-29 14:35:57 -0300

    implementing list users

--- bireme/main/views.py
+++ bireme/main/views.py
@@ -5,6 +5,7 @@ from django.utils.translation import ugettext_lazy as _
 from django.http import Http404, HttpResponse
 from django.core.urlresolvers import reverse
 from django.core.paginator import Paginator
+from django.contrib.auth.models import User
 from django.template import RequestContext
 from django.conf import settings
 from datetime import datetime
@@ -13,9 +14,38 @@ import mimetypes
 import os
 
 @login_required
-def index(request):
+def dashboard(request):
     
     user = request.user
     output = {}
 
     return render_to_response('main/index.html', output, context_instance=RequestContext(request))
+
+@login_required
+def users(request):
+
+    user = request.user
+    cc = request.user.profile.cooperative_center
+    output = {}
+
+    actions = {
+        'orderby': 'id',
+        'order': '+',
+        'page': 1,
+    }
+
+    # getting action parameters
+    for key in actions.keys():
+        if request.REQUEST.get(key):
+            actions[key] = request.REQUEST.get(key)
+
+    users = User.objects.filter(profile__cooperative_center=cc)
+    users = users.order_by(actions["orderby"])
+    if actions['order'] == "-":
+        users = users.order_by("%s%s" % (actions["order"], actions["orderby"]))
+
+    output['users'] = users
+    output['actions'] = actions
+    output['cc'] = cc
+
+    return render_to_response('main/users.html', output, context_instance=RequestContext(request))
