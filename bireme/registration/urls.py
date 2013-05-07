428cf5aec0eff4703c128c57e890ad9b79cfe733
moacirmoda <moa.moda@gmail.com>
2013-05-07 08:17:35 -0300

    implementing change profile

--- bireme/registration/urls.py
+++ bireme/registration/urls.py
@@ -5,10 +5,12 @@ urlpatterns = patterns('',
                        
      url(r'^login/$', auth_views.login, {'template_name': 'registration/login.html'}, name='auth_login'),
      url(r'^logout/$', auth_views.logout, {'template_name': 'registration/logout.html', 'next_page': '/'}, name='auth_logout'),
-     url(r'^password/change/$', auth_views.password_change, name='auth_password_change'), url(r'^password/change/done/$', auth_views.password_change_done, name='auth_password_change_done'),
-     url(r'^password/reset/$', auth_views.password_reset, name='auth_password_reset'), url(r'^password/reset/confirm/(?P<uidb36>[0-9A-Za-z]+)-(?P<token>.+)/$', auth_views.password_reset_confirm, name='auth_password_reset_confirm'),
+     url(r'^password/change/$', auth_views.password_change, { 'template_name': "registration/change-password.html"}, name='auth_password_change'), 
+     url(r'^password/change/done/$', auth_views.password_change_done, { 'template_name': "registration/change-password-done.html"}, name='auth_password_change_done'),
+     url(r'^password/reset/$', auth_views.password_reset, name='auth_password_reset'), 
+     url(r'^password/reset/confirm/(?P<uidb36>[0-9A-Za-z]+)-(?P<token>.+)/$', auth_views.password_reset_confirm, name='auth_password_reset_confirm'),
      url(r'^password/reset/complete/$', auth_views.password_reset_complete, name='auth_password_reset_complete'), 
      url(r'^password/reset/done/$', auth_views.password_reset_done, name='auth_password_reset_done'),
 
-     url(r'^change-profile/?$', "registration.views.change_profile" ),
+     url(r'^change-profile/?$', "registration.views.change_profile"),
 )
