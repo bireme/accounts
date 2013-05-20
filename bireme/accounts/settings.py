8e2b5ff2dd71ca5a3e485431b277882a37686930
Vinicius de Andrade <viniciusdeandrade@gmail.com>
2013-05-20 17:19:28 -0300

    Add locale path on settings

--- bireme/accounts/settings.py
+++ bireme/accounts/settings.py
@@ -46,6 +46,10 @@ LANGUAGES = (
     ('es', u'Espanhol'),
 )
 
+LOCALE_PATHS =(
+    os.path.join(PROJECT_ROOT_PATH, 'locale'),
+)
+
 SITE_ID = 1
 
 # If you set this to False, Django will make some optimizations so as not
