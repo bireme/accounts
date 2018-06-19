596956444f08a23d6aceca2937a79c7c6db59297
Vinicius de Andrade <viniciusdeandrade@gmail.com>
2018-06-19 10:41:26 -0300

    Ordena códigos de centros no form de rede. #8

--- bireme/main/forms.py
+++ bireme/main/forms.py
@@ -6,17 +6,17 @@ from models import *
 from django import forms
 
 class UserForm(forms.ModelForm):
-    
+
     class Meta:
         model = User
         fields = ('username', 'email', 'is_active')
 
     type = forms.ChoiceField(label=_("Type"), choices=Profile.USER_TYPE_CHOICES)
-        
+
     def __init__(self, *args, **kw):
         self.request = kw.pop("request")
         super(UserForm, self).__init__(*args, **kw)
-        # reordering 
+        # reordering
         self.fields.keyOrder = ['username', 'email', 'type', 'is_active']
 
         self.fields['type'].initial = self.instance.profile.type if self.instance.id else None
@@ -29,27 +29,27 @@ class UserForm(forms.ModelForm):
                 empty_label=None, initial=selected_cc, label=_("Cooperative Center") )
         else:
             # tk39 - allow advanced user to select cooperative center of users in their network (manage by his center)
-            
+
             ccs_networks_responsible = []
             # check networks that request user CC manages
             networks_managed = Network.objects.filter(responsible=self.request.user.profile.cooperative_center)
 
-            # create list with all CCs that user can view users 
+            # create list with all CCs that user can view users
             for net_managed in networks_managed:
                 ccs_networks_responsible.extend( [member.pk for member in net_managed.members.all()] )
-            
+
             # only show select when request user CC manages a network
             if len(ccs_networks_responsible) > 1:
                 selected_cc = self.instance.profile.cooperative_center if self.instance.id else None
-                self.fields['cc'] = forms.ModelChoiceField( 
+                self.fields['cc'] = forms.ModelChoiceField(
                         queryset=CooperativeCenter.objects.filter(pk__in=ccs_networks_responsible).order_by('code'),
-                        empty_label=None, initial=selected_cc, label=_("Cooperative Center") )                
+                        empty_label=None, initial=selected_cc, label=_("Cooperative Center") )
 
 
 
     def save(self, commit=True, *args, **kw):
         super(UserForm, self).save(commit=True, *args, **kw)
-        
+
         self.instance.profile.type = self.cleaned_data["type"]
         if 'cc' in self.cleaned_data:
             self.instance.profile.cooperative_center = self.cleaned_data["cc"]
@@ -63,6 +63,11 @@ class NetworkForm(forms.ModelForm):
         model = Network
         fields = ('country', 'topic', 'acronym', 'responsible', 'type')
 
+    def __init__(self, *args, **kw):
+        super(NetworkForm, self).__init__(*args, **kw)
+        # order cc codes
+        self.fields['responsible'] = forms.ModelChoiceField(queryset=CooperativeCenter.objects.order_by('code'),
+                                                            label=_("Responsible Cooperative Center") )
 
 class ServiceForm(forms.ModelForm):
 
@@ -77,4 +82,3 @@ class RoleForm(forms.ModelForm):
     class Meta:
         model = Role
         fields = ( 'name', 'acronym', 'description')
-
