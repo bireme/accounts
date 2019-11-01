19304a46b3bd4f69a3eae7318007b905ef3534be
Ronaldo Oliveira <rfdeoliveira.pmsp@gmail.com>
2019-11-01 12:35:58 -0300

    Acrescenta AuditLog aos models da app main

--- bireme/main/signals.py
+++ bireme/main/signals.py
@@ -1,15 +1,30 @@
-from django.db.models import signals
-from main.models import Network, CooperativeCenter, NetworkMembership
+#-*- coding: utf-8 -*-
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+
+from main.models import CooperativeCenter, Network, NetworkMembership, Profile
+
 
 def append_memberships(sender, instance, created, **kwargs):
     """
-        If the network type is national and was included your country,
-        automatically appends all centers that have that country too.
+    If the network type is national and was included your country,
+    automatically appends all centers that have that country too.
     """
     if created:
         if instance.type == 'national' and instance.country:
             for cc in CooperativeCenter.objects.filter(country=instance.country):
                 nm = NetworkMembership(network=instance, cooperative_center=cc)
                 nm.save()
-    
-signals.post_save.connect(append_memberships, sender=Network, dispatch_uid="some.unique.string.id")
+
+post_save.connect(append_memberships, sender=Network, dispatch_uid="some.unique.string.id")
+
+
+def create_profile(sender, instance, created, **kwargs):
+    """
+    Creates automatically and profile
+    """
+    if created:
+        profile = Profile(user=instance)
+        profile.save()
+
+post_save.connect(create_profile, sender=User, dispatch_uid="user_create_profile")
--- bireme/utils/models.py
+++ bireme/utils/models.py
@@ -1,47 +1,47 @@
-from django.db import models
-from django.utils.translation import ugettext_lazy as _
-from django.contrib.auth.models import User
+#-*- coding: utf-8 -*-
 from datetime import datetime
 
+from django.contrib.auth.models import User
+from django.db import models
+from django.utils.translation import ugettext_lazy as _
+
 LANGUAGES_CHOICES = (
-    ('en', 'English'), # default language 
+    ('en', 'English'), # default language
     ('pt-br', 'Brazilian Portuguese'),
     ('es', 'Spanish'),
 )
 
 class Generic(models.Model):
-
-    class Meta:
-        abstract = True
-
     created = models.DateTimeField(_("created"), default=datetime.now())
     updated = models.DateTimeField(_("updated"), default=datetime.now())
     creator = models.ForeignKey(User, null=True, blank=True, related_name="+")
     updater = models.ForeignKey(User, null=True, blank=True, related_name="+")
 
+    class Meta:
+        abstract = True
+
     def save(self):
         self.updated = datetime.now()
         super(Generic, self).save()
 
+
 class Country(Generic):
+    code = models.CharField(_('code'), max_length=55)
+    name = models.CharField(_('name'), max_length=255)
 
     class Meta:
         verbose_name = "Country"
         verbose_name_plural = "Countries"
-
-    code = models.CharField(_('code'), max_length=55)
-    name = models.CharField(_('name'), max_length=255)
 
     def __unicode__(self):
         return unicode(self.name)
 
 
 class CountryLocal(models.Model):
+    country = models.ForeignKey(Country, verbose_name=_("country"))
+    language = models.CharField(_("language"), max_length=10, choices=LANGUAGES_CHOICES[1:])
+    name = models.CharField(_("name"), max_length=255)
 
     class Meta:
         verbose_name = "Country Translation"
         verbose_name_plural = "Country Translations"
-
-    country = models.ForeignKey(Country, verbose_name=_("country"))
-    language = models.CharField(_("language"), max_length=10, choices=LANGUAGES_CHOICES[1:])
-    name = models.CharField(_("name"), max_length=255)
