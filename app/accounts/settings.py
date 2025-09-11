2005b6f07d458dda5ab8542cb8dbeec123ede4cb
Vinicius de Andrade <viniciusdeandrade@gmail.com>
2025-09-11 16:34:43 -0300

    refactor: migrate templates

--- app/accounts/settings.py
+++ app/accounts/settings.py
@@ -57,10 +57,11 @@ ROOT_URLCONF = 'accounts.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
+                'django.template.context_processors.i18n',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
