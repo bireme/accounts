ceb9eed1db0e36fd4596928740b280fc7332ae7e
Vinicius de Andrade <viniciusdeandrade@gmail.com>
2026-04-08 14:34:14 -0300

    refactor: removed old version scripts (bireme directory)

--- bireme/utils/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path, re_path
-
-from utils import views as utils_views
-
-app_name = 'utils'
-
-urlpatterns = [
-    re_path(r'^$', utils_views.cookie_lang, name='cookie_lang'),
-]
